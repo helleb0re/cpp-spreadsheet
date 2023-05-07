91314b6ed8b162fe720f3ff0f75d902aaf9c9eb0
helleb0re <34608677+helleb0re@users.noreply.github.com>
2023-05-07 14:51:56 +0300

    added some dependencies

--- design/cell.h
+++ design/cell.h
@@ -3,21 +3,91 @@
 #include "common.h"
 #include "formula.h"
 
+#include <optional>
+#include <unordered_set>
+
 class Cell : public CellInterface {
 public:
-    Cell();
-    ~Cell();
+    explicit Cell(SheetInterface &sheet);
 
     void Set(std::string text);
+
     void Clear();
 
     Value GetValue() const override;
+
     std::string GetText() const override;
 
+    std::vector<Position> GetReferencedCells() const override;
+
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+    class Impl {
+    public:
+        virtual ~Impl() = default;
+
+        virtual Value GetValue() const = 0;
+
+        virtual std::string GetText() const = 0;
+
+        virtual std::vector<Position> GetReferencedCells() const;
+
+        virtual std::optional<FormulaInterface::Value> GetCache() const;
+
+        virtual void ResetCache();
+    };
+
+    class EmptyImpl final : public Impl {
+    public:
+        Value GetValue() const override;
+
+        std::string GetText() const override;
+    };
+
+    class TextImpl final : public Impl {
+    public:
+        explicit TextImpl(std::string expression);
+
+        Value GetValue() const override;
+
+        std::string GetText() const override;
+
+    private:
+        std::string value_;
+    };
+
+    class FormulaImpl final : public Impl {
+    public:
+        explicit FormulaImpl(std::string expression, const SheetInterface &sheet);
+
+        Value GetValue() const override;
+
+        std::string GetText() const override;
+
+        std::vector<Position> GetReferencedCells() const override;
+
+        void ResetCache();
+
+        std::optional<FormulaInterface::Value> GetCache() const;
+
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+        const SheetInterface &sheet_;
+        mutable std::optional<FormulaInterface::Value> cache_;
+    };
+
+    void ContainCircularDependency(const std::vector<Position> &ref_cells) const;
+
+    void ContainCircularDependencyInner(const std::vector<Position> &ref_cells,
+                                        std::unordered_set<CellInterface *> &visited_cells) const;
+
+    void InvalidateCache();
+
+    void InvalidateCacheInner(std::unordered_set<Cell *> &visited_cells);
+
+    void UpdateDependencies(const std::vector<Position> &new_ref_cells);
+
+    SheetInterface &sheet_;
     std::unique_ptr<Impl> impl_;
+    std::unordered_set<Cell *> referenced_cells;
+    std::unordered_set<Cell *> dependent_cells;
 };
--- design/sheet.h
+++ design/sheet.h
@@ -4,31 +4,42 @@
 #include "common.h"
 
 #include <functional>
+#include <vector>
+#include <memory>
+#include <deque>
+#include <unordered_map>
 
 class Sheet : public SheetInterface {
 public:
-    ~Sheet();
+    ~Sheet() override = default;
 
     void SetCell(Position pos, std::string text) override;
 
-    const CellInterface* GetCell(Position pos) const override;
-    CellInterface* GetCell(Position pos) override;
+    const CellInterface *GetCell(Position pos) const override;
+
+    CellInterface *GetCell(Position pos) override;
 
     void ClearCell(Position pos) override;
 
     Size GetPrintableSize() const override;
 
-    void PrintValues(std::ostream& output) const override;
-    void PrintTexts(std::ostream& output) const override;
+    void PrintValues(std::ostream &output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
+    void PrintTexts(std::ostream &output) const override;
+
+    // Можете дополнить ваш класс нужными полями и методами
 
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
+    struct PositionHasher {
+        size_t operator()(const Position &pos) const {
+            return pos.row + pos.col * 37;
+        }
+    };
 
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+    std::unordered_map<Position, std::unique_ptr<Cell>, PositionHasher> sheet_;
+    Size printable_size_ = {0, 0};
+
+    static void PositionIsValid(Position pos);
+
+    void RecalculatePrintableSize();
 };
